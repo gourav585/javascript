5d2f2c05474c590df2fd3bd20c4630addbd129fd
Gourav Rajak <148696915+gourav585@users.noreply.github.com>
2024-11-01 20:22:31 +0530

    Add files via upload

---
+++ variables and data types/script.js
@@ -0,0 +1,27 @@
+console.log("hey this is nwe class of js")
+var a = 5;
+var b = 6;
+var c = "gourav";
+console.log(a+b)
+console.log(typeof a,typeof b, typeof c,)
+
+
+// var is the global variable
+// let is the local variable
+
+
+let x = "gourav";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z, typeof p,typeof q,typeof r)
+
+let o ={
+    name: "gourav",
+    "job code": 5600
+}
+console.log(o)
