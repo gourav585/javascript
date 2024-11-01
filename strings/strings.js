6b49bf11402a74cca8bbdc7b0b0d688d2ca0ccef
Gourav Rajak <148696915+gourav585@users.noreply.github.com>
2024-11-01 20:20:23 +0530

    Add files via upload

---
+++ strings/strings.js
@@ -0,0 +1,24 @@
+console.log("this is strings tutorial")
+let b = "gourav";
+console.log(b[0])
+console.log(b[1])
+console.log(b[2])
+console.log(b[3])
+console.log(b[4])
+console.log(b[5])
+
+console.log(b.length)
+
+let name = "gourav"
+let friend = "rohan"
+console.log("his name is " + name + " and his friend name is " + friend )
+console.log(`his name is ${name} and his friends name is ${friend}`)
+
+let a = "shivam";
+console.log(a.toLocaleUpperCase())
+console.log(a.toLocaleLowerCase())
+console.log(a.length)
+console.log(a.slice(1,5))
+
+console.log(a.replace("sh","77"))
+console.log(a.concat(b,"rohan"))
