156f503449a9eb1d7f70dc21afcd66081e7399f5
Gourav Rajak <148696915+gourav585@users.noreply.github.com>
2024-11-01 20:26:55 +0530

    Add files via upload

---
+++ conditional if else if else ladder/script.js
@@ -0,0 +1,27 @@
+if (condition) {
+    // block of code to be executed if the condition is true
+}
+
+
+if (condition) {
+    // block of code to be executed if the condition is true
+} else {
+    // block of code to be executed if the condition is false
+}
+
+if (condition1) {
+    // block of code to be executed if condition1 is true
+} else if (condition2) {
+    // block of code to be executed if the condition1 is false and condition2 is true
+} else {
+    // block of code to be executed if the condition1 is false and condition2 is false
+}
+
+let age = 45;
+
+if(age>18){
+    console.log("you can drive")
+}
+else{
+    console.log("you cannot drive")
+}
